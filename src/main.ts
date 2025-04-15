7b0d51430323d74c812f5adc8d6af5b8191a332d
Sebastian barrera <sebasbh2008@hotmail.com>
2025-04-15 12:52:26 -0500

    refactor ccr

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
 
   // Configuración de CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000/api",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
